f0cec5c22ab190c1ee7c9d116434420b40e9dc71
noir74y <noir74@yandex.ru>
2023-10-07 01:04:23 +0300

    feat: to externalize configuration (cont)

--- src/main/java/telegram/bot/googleSheet/GoogleSheetConfig.java
+++ src/main/java/telegram/bot/googleSheet/GoogleSheetConfig.java
@@ -10,11 +10,14 @@ import org.springframework.context.annotation.PropertySource;
 public class GoogleSheetConfig {
     private static String APPLICATION_NAME;
     private static String SERVICE_ACCOUNT_KEY_PATH;
+    private static String GOOGLE_SHEET_ID;
 
     public GoogleSheetConfig(@Value("${google.sheet.app.name}") String google_sheet_app_name,
-                             @Value("${google.sheet.service.account.key}") String google_sheet_service_account_key) {
+                             @Value("${google.sheet.service.account.key}") String google_sheet_service_account_key,
+                             @Value("${google.sheet.id}") String google_sheet_id) {
         APPLICATION_NAME = google_sheet_app_name;
         SERVICE_ACCOUNT_KEY_PATH = google_sheet_service_account_key;
+        GOOGLE_SHEET_ID = google_sheet_id;
     }
 
     public static String getApplicationName() {
@@ -23,6 +26,10 @@ public class GoogleSheetConfig {
 
     public static String getServiceAccountKeyPath() {
         return SERVICE_ACCOUNT_KEY_PATH;
+    }
+
+    public static String getGoogleSheetId() {
+        return GOOGLE_SHEET_ID;
     }
 
     @Override
