a946819e57a4da598b90bfa237f888a655397b34
noir74y <noir74@yandex.ru>
2023-10-07 11:27:33 +0300

    feat: base read/write methods for google sheet

--- src/main/java/telegram/bot/googleSheet/GoogleSheetConfig.java
+++ src/main/java/telegram/bot/googleSheet/GoogleSheetConfig.java
@@ -11,13 +11,19 @@ public class GoogleSheetConfig {
     private static String APPLICATION_NAME;
     private static String SERVICE_ACCOUNT_KEY_PATH;
     private static String GOOGLE_SHEET_ID;
+    private static String GOOGLE_SHEET_NAME_VOLUNTEERS;
+    private static String GOOGLE_SHEET_NAME_CONTACTS;
 
     public GoogleSheetConfig(@Value("${google.sheet.app.name}") String google_sheet_app_name,
                              @Value("${google.sheet.service.account.key}") String google_sheet_service_account_key,
-                             @Value("${google.sheet.id}") String google_sheet_id) {
+                             @Value("${google.sheet.id}") String google_sheet_id,
+                             @Value("${google.sheet.name.volunteers}") String google_sheet_name_volunteers,
+                             @Value("${google.sheet.name.contacts}") String google_sheet_name_contacts) {
         APPLICATION_NAME = google_sheet_app_name;
         SERVICE_ACCOUNT_KEY_PATH = google_sheet_service_account_key;
         GOOGLE_SHEET_ID = google_sheet_id;
+        GOOGLE_SHEET_NAME_VOLUNTEERS = google_sheet_name_volunteers;
+        GOOGLE_SHEET_NAME_CONTACTS = google_sheet_name_contacts;
     }
 
     public static String getApplicationName() {
@@ -31,6 +37,10 @@ public class GoogleSheetConfig {
     public static String getGoogleSheetId() {
         return GOOGLE_SHEET_ID;
     }
+
+    public static String getGoogleSheetNameVolunteers() { return GOOGLE_SHEET_NAME_VOLUNTEERS; }
+
+    public static String getGoogleSheetNameContacts() { return GOOGLE_SHEET_NAME_CONTACTS; }
 
     @Override
     public String toString() {
--- src/main/java/telegram/bot/googleSheet/GoogleSheetUtils.java
+++ src/main/java/telegram/bot/googleSheet/GoogleSheetUtils.java
@@ -5,6 +5,8 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import lombok.Data;
@@ -14,6 +16,9 @@ import org.springframework.stereotype.Component;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Data
@@ -23,10 +28,57 @@ public class GoogleSheetUtils {
 
     public GoogleSheetUtils() {
         initialize();
-        log.info("GoogleSheetUtils initialized");
+    }
+
+    public void writeCellValue(String sheetName, String cellAddress, String cellValue) {
+        try {
+            var range = sheetName + "!" + cellAddress;
+            var value = new ValueRange().setValues(List.of(List.of(cellValue)));
+            UpdateValuesResponse result = sheetService.spreadsheets().values()
+                    .update(GoogleSheetConfig.getGoogleSheetId(), range, value)
+                    .setValueInputOption("RAW")
+                    .execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readCellValue(String sheetName, String cellAddress) {
+        return readRangeValues(sheetName, cellAddress, cellAddress).get(0).get(0);
+    }
+
+    public List<List<String>> readRangeValues(String sheetName, String rangeBegin, String rangeEnd) {
+        List<List<String>> values = new LinkedList<>();
+        try {
+            var range = sheetName + "!" + rangeBegin + ":" + rangeEnd;
+
+            var objectsInTheRange = Optional
+                    .ofNullable(
+                            sheetService.spreadsheets()
+                                    .values()
+                                    .get(GoogleSheetConfig.getGoogleSheetId(), range)
+                                    .execute()
+                                    .getValues())
+                    .orElse(List.of(List.of("")));
+
+            objectsInTheRange
+                    .forEach(currentRowObject -> {
+                        var rowStringList = new LinkedList<String>();
+                        currentRowObject.forEach(currentCellObject -> rowStringList.add(Optional.ofNullable(currentCellObject).orElse("").toString()));
+                        values.add(rowStringList);
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return values;
     }
 
     private void initialize() {
+        connect();
+        prepareData();
+    }
+
+    private void connect() {
         GoogleCredentials googleCredentials;
         HttpRequestInitializer requestInitializer;
         NetHttpTransport netHttpTransport;
@@ -42,5 +94,10 @@ public class GoogleSheetUtils {
         this.sheetService = new Sheets.Builder(netHttpTransport, GsonFactory.getDefaultInstance(), requestInitializer)
                 .setApplicationName(GoogleSheetConfig.getApplicationName())
                 .build();
+    }
+
+    private void prepareData() {
+        writeCellValue(GoogleSheetConfig.getGoogleSheetNameVolunteers(), "C17", "test тест");
+        System.out.println(readCellValue(GoogleSheetConfig.getGoogleSheetNameVolunteers(), "C17"));
     }
 }
