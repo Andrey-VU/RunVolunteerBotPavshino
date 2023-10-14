352b0881cfd0ffe34461c32e3e4ab6c5bf9beb0b
noir74y <noir74@yandex.ru>
2023-10-15 00:36:01 +0300

    ref: add some comments to methods of StorageUtils

--- src/main/java/telegram/bot/storage/StorageUtils.java
+++ src/main/java/telegram/bot/storage/StorageUtils.java
@@ -8,12 +8,16 @@ public interface StorageUtils {
     // RX may absent (CY in such a case means "get all cell until last non-empty cell in Column Y")
     // CY may absent (RX in such a case means "get all cell until last non-empty cell in Row X")
 
+    // writes 'cellValue' into 'cellAddress' at 'sheetName'
     boolean writeCellValue(String sheetName, String cellAddress, String cellValue);
 
+    // writes martix 'values' starting from 'cellAddress' at 'sheetName'
     boolean writesValues(String sheetName, String cellAddress, List<List<Object>> values);
 
+    // reads list of String from 'rangeBegin' to 'rangeEnd' at 'sheetName'
     List<String> reagValuesList(String sheetName, String rangeBegin, String rangeEnd);
 
+    // reads matrix of String from 'rangeBegin' to 'rangeEnd' at 'sheetName'
     List<List<String>> readValuesRange(String sheetName, String rangeBegin, String rangeEnd);
 }
 
